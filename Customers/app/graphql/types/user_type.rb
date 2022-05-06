9868fe41633f9f7241adbce9b7dbe7f1be0bd3ad
GuiMarcelino <guilhermemarcelino_91@outlook.com>
2022-05-06 07:41:09 -0300

    criando os types graphql user e suggention, com o comando rails g graphql:object user

---
+++ Customers/app/graphql/types/user_type.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module Types
+  class UserType < Types::BaseObject
+    field :id, ID, null: false
+    field :email, String
+    field :name, String
+    field :type, Integer
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+  end
+end
