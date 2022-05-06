72725368ef69b9d4f15473ddbe8edd00341f1f55
GuiMarcelino <guilhermemarcelino_91@outlook.com>
2022-05-06 06:37:29 -0300

    gerando models

---
+++ Customers/db/migrate/20220506093031_create_suggestions.rb
@@ -0,0 +1,11 @@
+class CreateSuggestions < ActiveRecord::Migration[6.0]
+  def change
+    create_table :suggestions do |t|
+      t.belongs_to :user, null: false, foreign_key: true
+      t.string :title
+      t.text :body
+
+      t.timestamps
+    end
+  end
+end
